b2895940dbc32808cbfdb251e777134896922a29
Jeremy Fan <zheming.fan@gmail.com>
2020-02-14 12:11:08 -0700

    Created skeleton

---
+++ main.py
@@ -0,0 +1,49 @@
+import json
+import os
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+import requests
+import youtube_dl
+
+from secrets import spotify_token, spotify_user_id
+
+
+class CreatePlayList: 
+### Constructor
+
+	def __init__(self):
+
+## Pull youtube client and store information in a dictionary
+		self.youtube_client = self.get_youtube_client()
+		self.all_song_info = {}
+
+
+
+	def get_youtube_client(self):
+		pass
+
+	def get_liked_videos(self):
+		pass
+
+	def get_spotify_uri(self, song_name, artist):
+		pass
+
+	def create_playlist(self):
+		pass
+
+
+	def add_song_to_playlist(self):
+		pass
+
+
+if __name__ == '__main__':
+	cp = CreatePlayList()
+	cp.add_song_to_Playlist(): 
+
+
+
+
+
+
